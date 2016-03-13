3ef05141577038e854d9111d43d266f84b26b9c8
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-03-12 21:30:56 -0600

    Add UPDATE route

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.get("/todos", function (req, res) {
 });
 
 app.get("/todos/:id", function (req, res) {
-
 	var matchedTodo = _.findWhere(todos, {id: parseInt(req.params.id, 10)});
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -53,6 +52,36 @@ app.delete("/todos/:id", function (req, res) {
 		res.json(todos);
 	}
 });
+
+app.put("todos/:id", function (req, res){
+	var body = _.pick(req.body, ["description", "completed"]);
+	var matchedTodo = _.findWhere(todos, {id: parseInt(req.params.id, 10)});
+	var validAttributes = {};
+
+	if (!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty("completed")){
+		if (_.isBoolean(body.completed)){
+			validAttributes.completed = body.completed;
+		} else {
+			return res.status(400).send();
+		}
+	}
+
+	if (body.hasOwnProperty("description")){
+		if (_.isString(body.description) && body.description.trim().length > 0){
+			validAttributes.description = body.description.trim();
+		} else {
+			return res.status(400).send();
+		}
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
 
 
 app.listen(PORT, function() {

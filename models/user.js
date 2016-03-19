bfad1bb3b70ddd7b5f34ef2fe27acb21624eb01e
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-03-18 21:57:36 -0500

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcryptjs = require("bcryptjs"),
+	_        = require("underscore");
+
 module.exports = function (sequelize, DataTypes){
 	return sequelize.define("user", {
 		email: {
@@ -8,11 +11,21 @@ module.exports = function (sequelize, DataTypes){
 				isEmail: true
 			}
 		},
+		salt: { type: DataTypes.STRING },
+		hash: {	type: DataTypes.STRING },
 		password: {
-			type: DataTypes.STRING,
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7,100]
+			},
+			set: function (value){
+				var salt = bcryptjs.genSaltSync(10);
+				var hashedPassword = bcryptjs.hashSync(value, salt);
+
+				this.setDataValue("password",value);
+				this.setDataValue("salt", salt);
+				this.setDataValue("hash", hashedPassword);
 			}
 		}
 	}, {
@@ -21,6 +34,12 @@ module.exports = function (sequelize, DataTypes){
 				if (typeof user.email === "string"){
 					user.email = user.email.toLowerCase();
 				}
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function (){
+				var json = this.toJSON();
+				return _.pick(json, ["id", "email", "createdAt", "updatedAt"]);
 			}
 		}
 	});

3bc61fa28ad10fd5d13ea73704f94c07f9d1544f
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-03-31 12:56:34 -0500

    play with associations

--- playground/basic-sqlite.js
+++ playground/basic-sqlite.js
@@ -19,34 +19,43 @@ var Todo = sequelize.define("todo", {
 	}
 });
 
-sequelize.sync({force: true}).then(function (){
+var User = sequelize.define("user", {
+	email: Sequelize.STRING
+});
+
+//Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+	force: true
+}).then(function (){
 	console.log("Everything is synced");
 
-	Todo.create({
-		description: "Walk the dog",
-		completed: false
-	}).then (function (todo){
-		return Todo.create({
-			description: "Clean house"
+	//create association between user 1 and todo 1
+	User.create({
+		email: "user@example.com"
+	}).then(function (user){
+		Todo.create({
+			description: "Crean room"
+		}).then(function (todo){
+			user.addTodo(todo);
+		}, function (err){
+
 		});
-	}).then (function (todo){
-		
-		return Todo.findAll({
+	}, function (err){
+
+	});
+
+	//find all todos user 1 added
+	User.findById(1).then(function (user){
+		user.getTodos({
 			where: {
-				description: {
-					$like: "%clean%"
-				}
+				completed: false
 			}
-		});
-	}).then (function (foundTodos){
-		if (foundTodos){
-			foundTodos.forEach(function (todo){
+		}).then(function (todos){
+			todos.forEach(function (todo){
 				console.log(todo.toJSON());
-			});				
-		} else {
-			console.log("No Todo found!");
-		}
-	}).catch(function(err){
-		console.log(err);
+			});
+		});
 	});
 });
